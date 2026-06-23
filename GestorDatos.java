e820d3f72d849705a90c46c904dc5e87a8982763
snunez-dot <so.nunezg@duocuc.cl>
2026-06-22 21:44:18 -0400

    GestorDatos.java

--- GestorDatos.java
+++ GestorDatos.java
@@ -1,15 +1,11 @@
+package cl.llanquihuetour.data;
 
-package data;
-
-import model.Tours;
+import cl.llanquihuetour.model.Tours;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Gestor de datos de Tours, lee los datos desde un archivo.
- */
 public class GestorDatos {
 
     public ArrayList<Tours> cargarTours(String rutaArchivo) {
@@ -17,44 +13,43 @@ public class GestorDatos {
         ArrayList<Tours> listaTours = new ArrayList<>();
 
         try {
-            BufferedReader lector = new BufferedReader(
-                    new FileReader(rutaArchivo));
 
-        String linea;
+            BufferedReader lector =
+                    new BufferedReader(new FileReader(rutaArchivo));
 
-        while ((linea = lector.readLine()) != null) {
+            String linea;
 
-        String[] partes = linea.split(";");
+            while ((linea = lector.readLine()) != null) {
 
-            if (partes.length == 6) {
+                String[] partes = linea.split(";");
 
-                int id = Integer.parseInt(partes[0]);
+                if (partes.length == 6) {
+
+                    int id = Integer.parseInt(partes[0]);
                     String destino = partes[1];
                     String pais = partes[2];
                     String guia = partes[3];
                     int duracion = Integer.parseInt(partes[4]);
                     double precio = Double.parseDouble(partes[5]);
 
-        Tours tour = new Tours(
-            destino,
-            pais,
-            id,
-            guia,
-            duracion,
-            precio
-            );
+                    Tours tour = new Tours(
+                            destino,
+                            pais,
+                            id,
+                            guia,
+                            duracion,
+                            precio
+                    );
 
-    listaTours.add(tour);
-        }
-    }
+                    listaTours.add(tour);
+                }
+            }
 
             lector.close();
 
         } catch (IOException e) {
-            System.out.println("Error al leer el archivo: " + e.getMessage());
-
-        } catch (NumberFormatException e) {
-            System.out.println("Error al convertir datos numéricos: " + e.getMessage());
+            System.out.println("Error al leer el archivo: "
+                    + e.getMessage());
         }
 
         return listaTours;
