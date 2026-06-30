d0a7eb32d7a933be10eef942f4d8e4ec4fbac5d6
snunez-dot <so.nunezg@duocuc.cl>
2026-06-29 20:07:02 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-package cl.llanquihuetour.app;
+package cl.llanquihuetour.ui;
 
 import cl.llanquihuetour.data.GestorDatos;
+import cl.llanquihuetour.data.GestorServicios;
 import cl.llanquihuetour.model.Tours;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -14,6 +15,8 @@ public class Main {
         String rutaArchivo = "src/main/java/cl/llanquihuetour/files/tours.txt";
 
         GestorDatos gestor = new GestorDatos();
+        GestorServicios gestorServicios = new GestorServicios();
+
         ArrayList<Tours> tours = gestor.cargarTours(rutaArchivo);
 
         int opcion;
@@ -24,7 +27,8 @@ public class Main {
             System.out.println("1. Ver catálogo completo");
             System.out.println("2. Ver tours en Chile");
             System.out.println("3. Ver tours express");
-            System.out.println("4. Salir");
+            System.out.println("4. Servicios turísticos");
+            System.out.println("5. Salir");
             System.out.print("Seleccione una opción: ");
 
             opcion = teclado.nextInt();
@@ -67,15 +71,69 @@ public class Main {
 
                 case 4:
 
-                    System.out.println("\nSaliendo de Llanquihue Tours...");
+                    int opcionServicio;
+
+                    do {
+
+                        System.out.println("\n=== SERVICIOS TURÍSTICOS ===");
+                        System.out.println("1. Ver rutas gastronómicas");
+                        System.out.println("2. Ver paseos lacustres");
+                        System.out.println("3. Ver excursiones culturales");
+                        System.out.println("4. Ver todos los servicios");
+                        System.out.println("5. Volver");
+                        System.out.print("Seleccione una opción: ");
+
+                        opcionServicio = teclado.nextInt();
+
+                        switch (opcionServicio) {
+
+                            case 1:
+
+                                gestorServicios.mostrarRutasGastronomicas();
+                                break;
+
+                            case 2:
+
+                                gestorServicios.mostrarPaseosLacustres();
+                                break;
+
+                            case 3:
+
+                                gestorServicios.mostrarExcursionesCulturales();
+                                break;
+
+                            case 4:
+
+                                gestorServicios.mostrarTodos();
+                                break;
+
+                            case 5:
+
+                                System.out.println("Volviendo al menú principal...");
+                                break;
+
+                            default:
+
+                                System.out.println("Opción inválida.");
+
+                        }
+
+                    } while (opcionServicio != 5);
+
+                    break;
+
+                case 5:
+
+                    System.out.println("\nGracias por usar Llanquihue Tours.");
                     break;
 
                 default:
 
                     System.out.println("\nOpción inválida.");
+
             }
 
-        } while (opcion != 4);
+        } while (opcion != 5);
 
         teclado.close();
     }
